6a61ac5f31cf77efa30af4a3a20ef439a8c30cc9
rahul-sharma24 <rahul2003shukla@gmail.com>
2025-01-22 14:04:58 +0530

    first commit

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,20 @@
+import './page.css';
+import { Button } from '../components/ui/button';
+import { DialogDemo } from '../reusablecomponents/Dialog';
+import { useState } from 'react';
+
+export const LoginPage = () => {
+  const [isLoginOpen,setIsLoginOpen] =useState(false);
+  const [isSignUpOpen,setIsSignUpOpen] = useState(false);
+  return (
+    <div className='loginPage'>
+      <div className='h-[10vh] w-full flex items-center justify-center'>
+        <div className='bg-[rgb(0,0,0,0.3)] h-[80%] w-full flex justify-end items-center'>
+        <Button className='login' variant='primary' onClick={()=>{setIsLoginOpen(prev=>!prev)}}>Login</Button>
+          <Button className='signup' variant='primary'>Signup</Button>
+          <DialogDemo isOpen={isLoginOpen} setIsOpen={setIsLoginOpen}></DialogDemo>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/reusablecomponents/Dialog.jsx
@@ -0,0 +1,69 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import validator from 'validator'
+export const DialogDemo = ({ isOpen, setIsOpen }) => {
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const saveValues=(event,valueType)=>{
+    let value = event.target.value;
+    if(!value){
+      return "Error";
+    }
+    if(valueType=="password"){
+      if(validator.isStrongPassword(value))
+      setPassword(value)
+    }
+    else{
+      if(validator.isEmail(value))
+      setEmail(email);
+    }
+
+  }
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        {/* <Button variant="primary">Login</Button> */}
+      </DialogTrigger>
+      <DialogContent className="max-w-[65%] min-h-[40%] sm:max-w-[35%] md:max-w-[40%] xl:max-w-[30%] 2xl:max-w-[24%]">
+        <DialogHeader>
+          <DialogTitle className="text-[0.8rem] md:text-[1rem] 2xl:text-[1.2rem]">Login</DialogTitle>
+        </DialogHeader>
+        <div className="flex flex-col justify-center items-center gap-4">
+          {/* Email Input */}
+          <div className="flex flex-row w-full justify-between items-center">
+            <Label htmlFor="email" className="text-[0.7rem] md:text-[0.8rem] xl:text-[1rem] 2xl:text-[1.1rem]">Email</Label>
+            <Input
+              id="email"
+              value={email}
+              className=" w-[6.5rem] h-[1.7rem] text-sm md:w-[11rem] xl:w-[13rem] xl:h-7 px-2 2xl:w-[18rem] 2xl:h-[1.8rem]"
+            />
+          </div>
+          {/* Password Input */}
+          <div className="flex flex-row w-full justify-between items-center">
+            <Label htmlFor="password"className="text-[0.7rem] md:text-[0.8rem] xl:text-[1rem] 2xl:text-[1.1rem]">Password</Label>
+            <Input
+              id="password"
+              value={password}
+              onChange = {event=>saveValues(event)}
+              type="password"
+              className="  w-[6.5rem] h-[1.7rem] text-sm md:w-[11rem] xl:w-[13rem] xl:h-7 px-2 2xl:w-[18rem] 2xl:h-[1.8rem]"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit" className="text-[0.8rem] md:text-[1rem] xl:text-[1rem]">Sign In</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
